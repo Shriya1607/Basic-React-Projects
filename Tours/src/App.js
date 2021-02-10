61f02f469516677e761d67e4fe62dabb7662e480
Shriya1607 <guptashriya03@gmail.com>
2021-02-10 18:35:38 +0530

    Single Tour Displayed

---
+++ Tours/src/App.js
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from "react";
+import Loading from "./Loading";
+import Tours from "./Tours";
+
+const url = "https://course-api.com/react-tours-project";
+function App() {
+  return (
+    <>
+      <main>
+        <Tours />
+      </main>
+    </>
+  );
+}
+
+export default App;
